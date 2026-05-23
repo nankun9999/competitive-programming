6f399e37fbf42ac9603ca0cb07a3860e438f1ff3
nankun9999 <nankun9999@gmail.com>
2026-05-23 16:48:26 +0700

    Create 246.cpp

---
+++ marisaoj/246.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, low[100005], num[100005], timer;
+bool joint[100005];
+vector<int> adj[100005];
+void dfs(int u, int p) {
+    timer++;
+    num[u] = low[u] = timer;
+    int child = 0;
+    for (int v : adj[u]) {
+        if (v == p) {
+            continue;
+        }
+        if (!num[v]) {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            child++;
+            if (p == 0) {
+                if (child > 1) {
+                    joint[u] = true;
+                } 
+            } else if (low[v] >= num[u]) {
+                joint[u] = true;
+            }
+        } else {
+            low[u] = min(low[u], num[v]);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //freopen("TASK.INP", "r", stdin);
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!num[i]) {
+            dfs(i, 0);
+        }
+    }
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+        if (joint[i]) {
+            res++;
+        }
+    }
+    cout << res << "\n";
+    for (int i = 1; i <= n; i++) {
+        if (joint[i]) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
